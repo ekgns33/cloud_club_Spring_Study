96f1c91c98aab50e7f92592312bad58e8b43ef73
ekgns33 <ekgns00421@gmail.com>
2024-06-28 22:28:34 +0900

    feat: 강의수강완료

--- src/test/java/com/CloudClub/jpaStudy/service/ItemServiceTest.java
+++ src/test/java/com/CloudClub/jpaStudy/service/ItemServiceTest.java
@@ -1,14 +1,11 @@
 package com.CloudClub.jpaStudy.service;
 
-import static org.junit.Assert.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.CloudClub.jpaStudy.domain.item.Book;
 
 import jakarta.persistence.EntityManager;
 
@@ -21,18 +18,7 @@ public class ItemServiceTest {
 
 	@Test
 	public void update() throws Exception {
-	    //given
-	 	Book book = new Book();
 
-		 book.setAuthor("aaa");
-		 book.setIsbn("12312");
-		 book.setName("hi");
-		 book.setPrice(123123);
-		 ItemService.
-		Book book1 = em.find(Book.class, 1L);
-	    //when
-
-	    //then
 
 	}
 
